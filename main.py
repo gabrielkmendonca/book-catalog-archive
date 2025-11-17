790ca4e2646d09ba4f5c72af97dc06b7e40dcb8c
Gabriel Karlinski Mendonça <172616949+gabrielkmendonca@users.noreply.github.com>
2025-11-17 16:09:49 -0400

    code update

--- main.py
+++ main.py
@@ -1,113 +1,113 @@
-def cadastrar_catalogo(nome_arquivo):
-    catalogo = []
-    with open(nome_arquivo, "rt", encoding = "utf-8") as arquivo:
-        for linha in arquivo:
-            linha = linha.strip()
+def register_catalog(filename):
+    catalog = []
+    with open(filename, "rt", encoding = "utf-8") as archive:
+        for row in archive:
+            row = row.strip()
 
-            infos = linha.split(",")
+            infos = row.split(",")
 
-            titulo = infos[0]
-            autor = infos[1]
-            ano = int(infos[2])
-            genero = infos[3]
-            paginas = int(infos[4])
+            title = infos[0]
+            author = infos[1]
+            year = int(infos[2])
+            genre = infos[3]
+            pages = int(infos[4])
 
             ebook = {
-                "titulo" : titulo,
-                "autor" : autor,
-                "ano" : ano,
-                "genero" : genero,
-                "paginas" : paginas
+                "title" : title,
+                "author" : author,
+                "year" : year,
+                "genre" : genre,
+                "pages" : pages
             }
 
-            catalogo.append(ebook)
+            catalog.append(ebook)
             
-    return catalogo
+    return catalog
 
-def listar_genero(catalogo):
+def select_genre(catalog):
 
-    generos = []
+    genres = []
 
-    for ebook in catalogo:
-        if ebook["genero"] not in generos:
-            generos.append(ebook["genero"])
+    for ebook in catalog:
+        if ebook["genre"] not in genres:
+            genres.append(ebook["genre"])
 
-    print("Gêneros dos livros cadastrados no catálogo: \n")
+    print("Genres of books listed in the catalog: \n")
 
-    for gen in generos:
+    for gen in genres:
         print(f"—> {gen}")
 
-    genero_solicitado = input("\nInforme o gênero que gostaria de consultar: ")
+    selected_genre = input("\nSpecify the genre you would like to consult: ")
 
     print()
 
-    for ebook in catalogo:
-       if ebook["genero"].lower() == genero_solicitado.lower():
-           print(f"Título do Livro: {ebook["titulo"]} | Autor: {ebook["autor"]}")
+    for ebook in catalog:
+       if ebook["genre"].lower() == selected_genre.lower():
+           print(f"Book Title: {ebook["title"]} | Author: {ebook["author"]}")
 
-def calcular_media(catalogo):
-    total_paginas = 0
-    contador = 0
+def calculate_average(catalog):
+    total_pages = 0
+    counter = 0
 
-    for ebook in catalogo:
-        total_paginas += ebook["paginas"]
-        contador += 1
+    for ebook in catalog:
+        total_pages += ebook["pages"]
+        counter += 1
 
-    media = total_paginas / contador
+    average = total_pages / counter
 
-    print("Média de páginas dos livros do catálogo: \n")
-    print(f"{media} páginas")
+    print("Average number of pages in the books catalog: \n")
+    print(f"{average} pages")
 
-def ordenar_ano(catalogo):
-    mais_antigo = catalogo[0]
+def sort_year(catalog):
+    oldest_book = catalog[0]
 
-    for ebook in catalogo:
-        if ebook["ano"] < mais_antigo["ano"]:
-            mais_antigo = ebook
+    for ebook in catalog:
+        if ebook["year"] < oldest_book["year"]:
+            oldest_book = ebook
 
-    print("Livro mais antigo: \n")
-    print(f"Título do Livro: {mais_antigo["titulo"]} | Ano de Publicação: {mais_antigo["ano"]} ")
+    print("Oldest book: \n")
+    print(f"Book Title: {oldest_book["title"]} | Year of Publication: {oldest_book["year"]} ")
 
-def limitar_paginas(catalogo):
-    limitador = int(input("Informe a quantidade mínima de páginas para a consulta: "))
+def min_pages(catalog):
+    min = int(input("Specify the minimum number of pages required for the query: "))
 
-    print(f"\nLivros do catálogo com mais de {limitador} páginas:\n")
+    print(f"\nBooks in the catalog with more than {min} pages:\n")
 
-    for ebook in catalogo:
-        if ebook["paginas"] >= limitador:
-            print(f"Título do Livro: {ebook["titulo"]} | Número de Páginas: {ebook["paginas"]}")
+    for ebook in catalog:
+        if ebook["pages"] >= min:
+            print(f"Book Title: {ebook["title"]} | Number of Pages: {ebook["pages"]}")
 
-def ordernar_titulo(catalogo):
-    catalogo_ordenado = catalogo[:]
+def sort_title(catalog):
+    sorted_catalog = catalog[:]
 
-    tamanho = len(catalogo_ordenado)
-    trocou = True
+    size = len(sorted_catalog)
+    changed = True
     
-    while trocou:
-        trocou = False
+    while changed:
+        changed = False
 
-        for i in range(tamanho - 1):
-            if catalogo_ordenado[i]["titulo"].lower() > catalogo_ordenado[i + 1]["titulo"].lower():
-                suporte = catalogo_ordenado[i]
-                catalogo_ordenado[i] = catalogo_ordenado[i + 1]
-                catalogo_ordenado[i + 1] = suporte
-                trocou = True
+        for i in range(size - 1):
+            if sorted_catalog[i]["title"].lower() > sorted_catalog[i + 1]["title"].lower():
+                sup = sorted_catalog[i]
+                sorted_catalog[i] = sorted_catalog[i + 1]
+                sorted_catalog[i + 1] = sup
+                changed = True
 
-    print("Catálogo de livros ordenado por ordem alfabética crescente:\n")
+    print("Catalog of books in alphabetical order:\n")
 
-    for ebook in catalogo_ordenado:
-        print(f"Título do Livro: {ebook["titulo"]} | Autor: {ebook["autor"]}")
+    for ebook in sorted_catalog:
+        print(f"Book Title: {ebook["title"]} | Author: {ebook["author"]}")
     
     print()
 
-nome_arquivo = "books_catalog.txt"
-catalogo = cadastrar_catalogo(nome_arquivo) 
-listar_genero(catalogo) 
+filename = "books_catalog.txt"
+catalog = register_catalog(filename) 
+select_genre(catalog) 
 print("\n-----------------------------------------------------------------------------------------------------------------------------------\n")
-calcular_media(catalogo) 
+calculate_average(catalog) 
 print("\n-----------------------------------------------------------------------------------------------------------------------------------\n")
-ordenar_ano(catalogo)   
+sort_year(catalog)   
 print("\n-----------------------------------------------------------------------------------------------------------------------------------\n")
-limitar_paginas(catalogo) 
+min_pages(catalog) 
 print("\n-----------------------------------------------------------------------------------------------------------------------------------\n")
-ordernar_titulo (catalogo) 
+sort_title (catalog) 
